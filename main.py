f78f0cad4eaa539cf179da1adef0c9230fbd67c7
rstolberg <rileystolberg@gmail.com>
2025-05-23 13:06:48 -0700

    function creation

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ def get_book_text():
     with open('books/frankenstein.txt', 'r') as f:
         return f.read()
 
+def get_book_words():
+    book_words = get_book_text().split()
+    return len(book_words)
+
+
 def main():
-    print(get_book_text())
+    print(f"75767 words found in the document")
 
 
 main()
